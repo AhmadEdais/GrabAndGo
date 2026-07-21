68fb658c58468597ea481ee52c783eb921cba969
Ahmad Edais <ahmadadais.aa@gmail.com>
2026-07-21 18:27:38 +0300

    Disable HTTPS redirection in production container

--- GrabAndGo/Program.cs
+++ GrabAndGo/Program.cs
@@ -191,13 +191,14 @@ builder.Services.AddAuthorization();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-//if (app.Environment.IsDevelopment())
-//{
-    app.UseSwagger();
-    app.UseSwaggerUI();
 
-//}
-app.UseHttpsRedirection();
+app.UseSwagger();
+app.UseSwaggerUI();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseHttpsRedirection();
+}
 app.UseCors("FrontendPolicy");
 app.UseAuthentication();
 app.UseAuthorization();
